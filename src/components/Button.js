dde4a391345d05e2fec593be3a5acb53d120ca3e
Bipul Chaudhary <bipulchau@gmail.com>
2021-10-26 15:26:08 -0400

    using classNames function in place of if statement

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,22 @@
 import React from "react";
+import classNames from "classnames";
+
 
 import "components/Button.scss";
 
 export default function Button(props) {
-  let buttonClass = "button";
-  if (props.confirm) {
-    buttonClass += " button--confirm";
-  }
+//   let buttonClass = "button";
+//   if (props.confirm) {
+//     buttonClass += " button--confirm";
+//   }
 
-  if (props.danger) {
-    buttonClass += " button--danger";
-  }
+//   if (props.danger) {
+//     buttonClass += " button--danger";
+//   }
+let buttonClass =classNames("button",{
+   "button--confirm":props.confirm,
+   "button--danger":props.danger
+})
   return (<button 
   className={buttonClass}
   onClick={props.onClick}
