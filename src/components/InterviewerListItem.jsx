11155d317c2275ec7f4624c170a89e9386b3cd0f
Bipul Chaudhary <bipulchau@gmail.com>
2021-10-27 14:21:02 -0400

    replaced hardcoded values with props

--- src/components/InterviewerListItem.jsx
+++ src/components/InterviewerListItem.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import "components/InterviewerListItem.scss"
 
 
-const InterviewerListItem = () => {
+const InterviewerListItem = (props) => {
+  const{id, name, avatar} = props;
   return(
     <li className="interviewers__item">
   <img
     className="interviewers__item-image"
-    src="https://i.imgur.com/LpaY82x.png"
-    alt="Sylvia Palmer"
+    src={avatar}
+    alt={name}
   />
-  Sylvia Palmer
+  {name}
 </li>
   )
 }
 
 export default InterviewerListItem;
+
