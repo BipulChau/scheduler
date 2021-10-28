ac3bdacbe47d7027d02b400511ed21794bcf2554
Bipul Chaudhary <bipulchau@gmail.com>
2021-10-28 19:28:47 -0400

    requesting days api using axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,30 +1,9 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import axios from "axios";
-
-
-// Days data ******************************************************
-
-// const days = [
-//   {
-//     id: 1,
-//     name: "Monday",
-//     spots: 2,
-//   },
-//   {
-//     id: 2,
-//     name: "Tuesday",
-//     spots: 5,
-//   },
-//   {
-//     id: 3,
-//     name: "Wednesday",
-//     spots: 0,
-//   },
-// ];
 
 // Appointments Data****************************************************************************
 
@@ -38,12 +17,12 @@ const appointments = [
     time: "1pm",
     interview: {
       student: "Lydia Miller-Jones",
-      interviewer:{
+      interviewer: {
         id: 3,
         name: "Sylvia Palmer",
         avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
+      },
+    },
   },
   {
     id: 3,
@@ -54,63 +33,60 @@ const appointments = [
     time: "3pm",
     interview: {
       student: "Archie Andrews",
-      interviewer:{
+      interviewer: {
         id: 4,
         name: "Cohana Roy",
         avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
+      },
+    },
   },
   {
     id: 5,
     time: "4pm",
-  }
+  },
 ];
 
 // Application Component ********************************************************************************//
 
 export default function Application(props) {
-  const [day,setDay] = useState("")
-  const [days,setDays] = useState([])
-  console.log(day)
+  const [day, setDay] = useState("");
+  const [days, setDays] = useState([]);
+  console.log(day);
 
   useEffect(() => {
-    axios.get("/api/days")
-    .then(res => {
+    axios.get("/api/days").then((res) => {
       console.log(res.data);
-      setDays([...res.data])
-    })
-  },[])
+      setDays([...res.data]);
+    });
+  }, []);
   // creating an array of appointment date******************//
-const eachAppointment= appointments.map(appointment => <Appointment key ={appointment.id} {...appointment}/>)
+  const eachAppointment = appointments.map((appointment) => (
+    <Appointment key={appointment.id} {...appointment} />
+  ));
 
   return (
     <main className="layout">
       <section className="sidebar">
         {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
         <img
-  className="sidebar--centered"
-  src="images/logo.png"
-  alt="Interview Scheduler"
-/>
-<hr className="sidebar__separator sidebar--centered" />
-<nav className="sidebar__menu">
-  <DayList
-  days={days}
-  value={day}
-  onChange={setDay}
-  />
-</nav>
-<img
-  className="sidebar__lhl sidebar--centered"
-  src="images/lhl.png"
-  alt="Lighthouse Labs"
-/>
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          <DayList days={days} value={day} onChange={setDay} />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
         {eachAppointment}
-        <Appointment key="last" time="5pm"/>
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
