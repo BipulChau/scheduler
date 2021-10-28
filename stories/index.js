bb732f7e47ac63062cfaacd13cad307037be2c94
Bipul Chaudhary <bipulchau@gmail.com>
2021-10-27 20:34:03 -0400

    created Confirm component and its story

--- stories/index.js
+++ stories/index.js
@@ -12,8 +12,10 @@ import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
-import Empty from "components/Appointment/Empty"
-import Show from "components/Appointment/Show"
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm"
+import { act } from "@testing-library/react";
 
 //Button................................................................................................
 storiesOf("Button", module)
@@ -152,6 +154,14 @@ storiesOf("Appointment", module)
   })
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
-  .add("Header", () => <Header time="11pm"/>)
-  .add("Empty", ()=><Empty onAdd={action("onAdd")} />)
-  .add("Show", ()=><Show student={"Bipul Chaudhary"} interviewer={interviewer} onEdit={action("onEdit")} onDelete={action("onDelete")}/>)
+  .add("Header", () => <Header time="11pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add("Show", () => (
+    <Show
+      student={"Bipul Chaudhary"}
+      interviewer={interviewer}
+      onEdit={action("onEdit")}
+      onDelete={action("onDelete")}
+    />
+  ))
+  .add("Confirm", () => <Confirm message={"Delete the appointment"} onConfirm={action("onConfirm")} onCancel={action("onCancel")}/>)
