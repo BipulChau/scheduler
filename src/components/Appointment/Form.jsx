67000bc2a01191d36885fc3c2a3cb7122d21b65b
Bipul Chaudhary <bipulchau@gmail.com>
2021-10-28 07:00:53 -0400

    Create Reset and Cancel function which will set the interviewer and student value to null or empty string

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -5,7 +5,18 @@ import InterviewerList from "components/InterviewerList";
 const Form = (props) => {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
-  
+
+  //Reset Function that sets student and interviewer to ""
+  const reset = () => {
+    setStudent("");
+    setInterviewer(null);
+  };
+
+  // Function that calls both reset function and props.onCancel
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -17,15 +28,23 @@ const Form = (props) => {
             type="text"
             placeholder="Enter Student Name"
             value={student}
-            onChange={e => setStudent(e.target.value)}
+            onChange={(e) => setStudent(e.target.value)}
           />
         </form>
-        <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer}/>
+        <InterviewerList
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
+        />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
-          <Button confirm onClick={props.onSave}>Save</Button>
+          <Button danger onClick={cancel}>
+            Cancel
+          </Button>
+          <Button confirm onClick={props.onSave}>
+            Save
+          </Button>
         </section>
       </section>
     </main>
