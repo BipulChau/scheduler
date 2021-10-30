03c1b2e42f9519d6792ae21f8b9309b2059ab577
Bipul Chaudhary <bipulchau@gmail.com>
2021-10-30 17:41:09 -0400

    Imported useVisualMode into Applicatin/index.js

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -3,14 +3,22 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
 
 const Appointment = (props) => {
-  const {time, interview} = props;
+  const { time, interview } = props;
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
   return (
     <article className="appointment">
-      <Header time={time}/>
+      <Header time={time} />
       {/* {props.time ? `Appointment at ${props.time}` : "No Appointments"} */}
-      {interview?<Show student={interview.student} interviewer={interview.interviewer}/>:<Empty/>}
+      {interview ? (
+        <Show student={interview.student} interviewer={interview.interviewer} />
+      ) : (
+        <Empty />
+      )}
     </article>
   );
 };
