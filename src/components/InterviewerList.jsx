6348666cfde0c9a46e1c8abccf02b23a86f7b510
Bipul Chaudhary <bipulchau@gmail.com>
2021-11-02 14:49:08 -0400

    installed prop-types and imported it into InterviewerList.jsx

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -1,21 +1,23 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
-import "components/InterviewerList.scss"
+import "components/InterviewerList.scss";
+import PropTypes from "prop-types";
 
 const InterviewerList = (props) => {
-  const{interviewers, onChange, value} = props;
-  const interviewersListItem = interviewers.map(interviewere =>{
-    return (<InterviewerListItem
-    
-    key = {interviewere.id}
-    name = {interviewere.name}
-    avatar = {interviewere.avatar}
-    setInterviewer = {e=>onChange(interviewere.id)}
-    selected = {interviewere.id === value}
-    />)
-  })
+  const { interviewers, onChange, value } = props;
+  const interviewersListItem = interviewers.map((interviewere) => {
+    return (
+      <InterviewerListItem
+        key={interviewere.id}
+        name={interviewere.name}
+        avatar={interviewere.avatar}
+        setInterviewer={(e) => onChange(interviewere.id)}
+        selected={interviewere.id === value}
+      />
+    );
+  });
 
-  console.log("interviewerListItem", interviewersListItem) //test
+  console.log("interviewerListItem", interviewersListItem); //test
 
   return (
     <section className="interviewers">
@@ -25,4 +27,8 @@ const InterviewerList = (props) => {
   );
 };
 
-export default InterviewerList; 
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired,
+};
+
+export default InterviewerList;
