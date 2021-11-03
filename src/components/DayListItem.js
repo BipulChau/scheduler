291447cf51530a87a071244ec0d2e10355faf31f
Bipul Chaudhary <bipulchau@gmail.com>
2021-11-03 17:53:13 -0400

    Cypress Test

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -12,7 +12,11 @@ export default function DayListItem(props) {
 };
 
   return(
-    <li className={dayClass.replace(/ /g,'')} onClick={()=>props.setDay(props.name)} >
+    <li 
+    className={dayClass.replace(/ /g,'')} 
+    onClick={()=>props.setDay(props.name)}  
+    data-testid="day"
+    >
       <h2>{props.name}</h2>
       <h3>{formatSpots(props.spots)} remaining</h3>
     </li>
